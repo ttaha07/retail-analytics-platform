ec5365a04458ee72d4e6efbe549cf784478e69ee
ttaha07 <ttaha015@gmail.com>
2026-06-22 04:01:16 +0000

    Replace Python runner content with SQL source files

--- sql/silver/silver.sql
+++ sql/silver/silver.sql
@@ -1,30 +1,3 @@
-# sql/silver/run_silver.py
-
-import snowflake.connector
-import os
-
-conn = snowflake.connector.connect(
-    user=os.environ["SNOWFLAKE_USER"],
-    password=os.environ["SNOWFLAKE_PASSWORD"],
-    account=os.environ["SNOWFLAKE_ACCOUNT"],
-    warehouse=os.environ["SNOWFLAKE_WAREHOUSE"],
-    database=os.environ["SNOWFLAKE_DATABASE"],
-    schema=os.environ["SNOWFLAKE_SCHEMA"]
-)
-
-cur = conn.cursor()
-
-print("Running Silver Layer...")
-
-files = [
-    "sql/silver/create_silver_tables.sql",
-    "sql/silver/load_silver_tables.sql",
-    "sql/silver/validation.sql"
-]
-
-for fpath in files:
-    with open(fpath, "r") as f:
-        cur.execute(f.read())
-        print(f"Executed {fpath}")
-
-print("Silver Complete")
+!source sql/silver/create_silver_tables.sql
+!source sql/silver/load_silver_tables.sql
+!source sql/silver/validation.sql
